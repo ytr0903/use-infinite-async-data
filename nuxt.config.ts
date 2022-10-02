b7579ae5eb66c82b286cdf0b1b31e2b74a6e7d3d
ytr0903 <35318519+ytr0903@users.noreply.github.com>
2022-10-02 22:41:30 +0900

    add element-plus / pokemon.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 export default defineNuxtConfig({
-
-})
+  vite: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
+  build: {
+    transpile: ["element-plus", "pokemon.json"],
+  },
+});
