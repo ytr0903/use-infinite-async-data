6235bde06e7129811e8a6ca82c9c54f18ebc26e3
ytr0903 <35318519+ytr0903@users.noreply.github.com>
2022-10-03 01:01:59 +0900

    Loader Component with vueuse

---
+++ components/Loader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div ref="target" />
+</template>
+
+<script lang="ts" setup>
+const target = ref(null);
+const targetIsVisible = useElementVisibility(target);
+const emits = defineEmits(["load"]);
+watchEffect(() => {
+  if (targetIsVisible.value) emits("load");
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: ["@vueuse/nuxt"],
   build: {
     transpile: ["pokemon.json"],
   },
