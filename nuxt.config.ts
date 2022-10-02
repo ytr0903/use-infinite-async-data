122b859eeba2a15eedc26a4a114eb527bfab4b55
ytr0903 <35318519+ytr0903@users.noreply.github.com>
2022-10-02 23:26:58 +0900

    remove element-plus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 export default defineNuxtConfig({
-  vite: {
-    plugins: [
-      AutoImport({
-        resolvers: [ElementPlusResolver()],
-      }),
-      Components({
-        resolvers: [ElementPlusResolver()],
-      }),
-    ],
-  },
   build: {
-    transpile: ["element-plus", "pokemon.json"],
+    transpile: ["pokemon.json"],
   },
 });
