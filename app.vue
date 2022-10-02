8df7131efb840bbc2eb33e9d562166cfecc46621
ytr0903 <35318519+ytr0903@users.noreply.github.com>
2022-10-03 01:02:27 +0900

    paging

--- app.vue
+++ app.vue
@@ -1,39 +1,47 @@
 <template>
   <div>
-    <h1>useInfiniteAsyncData</h1>
-    <p>
-      <code>useAsyncData()</code> にページネーション機能を追加した
-      <code>useInfiniteAsyncData()</code> のデモページです。
-    </p>
-  </div>
-  <div class="box">
-    <template v-if="data">
-      <template v-for="(poke, i) in data">
-        <div class="box-card">
-          <img
-            :alt="poke.name"
-            :src="getImagePath(poke.number)"
-            class="image"
-          />
-          <h4 style="margin: 0">{{ poke.number }}. {{ poke.name }}</h4>
+    <div class="header">
+      <h1>useInfiniteAsyncData</h1>
+      <p>
+        <code>useAsyncData()</code> にページネーション機能を追加した
+        <code>useInfiniteAsyncData()</code> のデモページです。
+      </p>
+    </div>
+    <div class="box">
+      <div class="box-card" v-for="(poke, i) in data" :key="i">
+        <img :alt="poke.name" :src="getImagePath(poke.number)" class="image" />
+        <h4 style="margin: 0">{{ poke.number }}. {{ poke.name }}</h4>
+      </div>
+      <template v-if="pending">
+        <div class="box-card" v-for="i in 10">
+          <div class="skelton"></div>
+          <h4 style="margin: 0">Now Loading...</h4>
         </div>
       </template>
-    </template>
+      <Loader v-if="!pending && !isCompleted" @load="setSize(size + 1)" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const { data, pending } = useAsyncData(
-  () => {
+const {
+  data: dataList,
+  pending,
+  size,
+  setSize,
+} = useInfiniteAsyncData(
+  (i: number) => [i],
+  (i: number) => {
     return $fetch<{
+      next: string;
       results: { name: string; url: string }[];
     }>("https://pokeapi.co/api/v2/pokemon/", {
-      query: { limit: 10, offset: 0 },
+      query: { limit: 10, offset: (i - 1) * 10 },
     });
   },
   {
-    transform: ({ results }) => {
-      return results.map((val) => ({
+    transform: (res) => {
+      return res.results.map((val) => ({
         name: val.name,
         url: val.url,
         number: Number(`${val.url}`.split("/")?.at(-2)),
@@ -41,6 +49,9 @@ const { data, pending } = useAsyncData(
     },
   }
 );
+
+const data = computed(() => dataList.value?.flat() ?? null);
+const isCompleted = computed(() => data.value.length < size.value * 10);
 
 const getImagePath = (i: number) => {
   return `node_modules/pokemon.json/images/${`${i}`.padStart(3, "0")}.png`;
@@ -61,5 +72,20 @@ const getImagePath = (i: number) => {
 
 .image {
   width: 100%;
+}
+
+@keyframes skelton {
+  0% {
+    background: #ccc;
+  }
+  100% {
+    background: #ddd;
+  }
+}
+.skelton {
+  display: inline-block;
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  animation: skelton 0.8s ease 0s infinite alternate;
 }
 </style>
---
+++ composables/useInfiniteAsyncData.ts
@@ -0,0 +1,106 @@
+import type { AsyncData } from "nuxt/dist/app";
+import type {
+  AsyncDataOptions,
+  KeyOfRes,
+  PickFrom,
+  _Transform,
+} from "nuxt/dist/app/composables/asyncData";
+import type { Ref } from "vue";
+
+type ArgumentsTuple = [...any[]];
+
+type InfiniteAsyncDataFetcher<DataT, ParamT extends ArgumentsTuple> = (
+  ...args: ParamT
+) => Promise<DataT>;
+
+type InfiniteAsyncDataKeyLoader<DataT, ParamT extends ArgumentsTuple> = (
+  index: number,
+  previousPageData: DataT | null
+) => [...Parameters<InfiniteAsyncDataFetcher<DataT, ParamT>>];
+
+/** configuration */
+interface InfiniteAsyncDataOptions<
+  DataT = any,
+  Transform extends _Transform<DataT, any> = _Transform<DataT, DataT>,
+  PickKeys extends KeyOfRes<_Transform> = KeyOfRes<Transform>
+> extends AsyncDataOptions<DataT, Transform, PickKeys> {
+  initialSize?: number;
+  // revalidateAll?: boolean
+  // persistSize?: boolean
+  // revalidateFirstPage?: boolean
+  // fetcher?: InfiniteAsyncDataFetcher<DataT>
+}
+
+/** response */
+interface InfiniteAsyncData<DataT = any, DataE = Error>
+  extends AsyncData<DataT[], DataE> {
+  size: Ref<number>;
+  setSize: (
+    size: number | ((size: number) => number)
+  ) => Promise<DataT[] | null>;
+}
+
+export const useInfiniteAsyncData = <
+  ParamT extends ArgumentsTuple,
+  DataT,
+  DataE = Error,
+  Transform extends _Transform<DataT> = _Transform<DataT, DataT>,
+  PickKeys extends KeyOfRes<Transform> = KeyOfRes<Transform>
+>(
+  getKey: InfiniteAsyncDataKeyLoader<
+    PickFrom<ReturnType<Transform>, PickKeys>,
+    ParamT
+  >,
+  fetcher: InfiniteAsyncDataFetcher<DataT, ParamT>,
+  options: InfiniteAsyncDataOptions<DataT, Transform, PickKeys> = {
+    initialSize: 1,
+  }
+): InfiniteAsyncData<
+  PickFrom<ReturnType<Transform>, PickKeys>,
+  DataE | true | null
+> => {
+  const size = ref(options.initialSize ?? 1);
+  const setSize = async (arg: number | ((size: number) => number)) => {
+    size.value = typeof arg === "number" ? arg : arg(size.value);
+    await _refresh();
+    return dataList.value;
+  };
+
+  const dataList = useState<PickFrom<ReturnType<Transform>, PickKeys>[]>();
+
+  const {
+    data,
+    pending,
+    refresh: _refresh,
+    execute,
+    error,
+  } = useAsyncData<DataT, DataE, Transform, PickKeys>(() => {
+    return fetcher(...getKey(size.value, dataList.value?.at(-1) ?? null));
+  }, options);
+
+  const refresh = async () => {
+    dataList.value = [];
+    await setSize(1);
+  };
+
+  watch(
+    () => data.value,
+    (val) => {
+      if (!dataList.value) dataList.value = [];
+      if (val) dataList.value[size.value - 1] = val;
+    },
+    { immediate: true }
+  );
+
+  const asyncData = {
+    data: dataList,
+    pending,
+    refresh,
+    execute,
+    error,
+    size,
+    setSize,
+  };
+  const asyncDataPromise = Promise.resolve().then(() => asyncData);
+  return { ...asyncData, ...asyncDataPromise };
+};
